a071cd7aa104c6f202824a19630370fedbda8b96
5umitra <gaudmeet00@gmail.com>
2024-07-24 17:55:54 +0530

    Register Ctrl bugs Fixed!!

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -25,14 +25,7 @@ const userSchema = new Schema(
             trim: true,
             index: true
         },
-        username : {
-            type: String,
-            required: true,
-            unique: true,
-            lowercase: true,
-            trim: true,
-            index: true
-        },
+
         avatar : {
             type: String,   //cloudinary
             required: true
